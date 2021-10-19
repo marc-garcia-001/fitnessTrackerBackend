975e91eb30534f13dd2b91225ec9fb5be7b66f92
Marc Garcia <garcia.marc.001@gmail.com>
2021-10-19 13:01:16 -0400

    formatted DB

--- db/seedData.js
+++ db/seedData.js
@@ -30,20 +30,24 @@ async function createTables() {
     try {
       console.log("Starting to build tables...");
       await client.query(`
+      
       CREATE TABLE users
       id SERIAL PRIMARY KEY
       username VARCHAR(255) UNIQUE NOT NULL
       password VARCHAR(255) NOT NULL
+
       CREATE TABLE activities
       id SERIAL PRIMARY KEY
       name VARCHAR(255) UNIQUE NOT NULL
       description TEXT NOT NULL
+
       CREATE TABLE routines
       id SERIAL PRIMARY KEY
       "creatorId" INTEGER FOREIGN KEY
       "isPublic" BOOLEAN DEFAULT false
       name VARCHAR(255) UNIQUE NOT NULL
       goal TEXT NOT NULL
+
       CREATE TABLE routine_activities
       id SERIAL PRIMARY KEY
       "routineId" INTEGER FOREIGN KEY UNIQUE
