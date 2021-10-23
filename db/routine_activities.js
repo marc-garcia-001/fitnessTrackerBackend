d0997d7a48a5a965cfe58c93719d5758e9f1f762
Marc Garcia <garcia.marc.001@gmail.com>
2021-10-23 14:35:59 -0400

    routine_activities

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -45,6 +45,63 @@ async function addActivityToRoutine({
   }
 }
 
+async function updateRoutineActivity({ id, count, duration }) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+      UPDATE routine_activities
+      SET count=$2, duration=$3
+      WHERE id=$1
+      RETURNING *;
+    `,
+      [id, count, duration]
+    );
+
+    return activity;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function destroyRoutineActivity(id) {
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+    DELETE
+    FROM routine_activities
+    WHERE id=$1
+    RETURNING *;
+    `,
+      [id]
+    );
+
+    return activity;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getRoutineActivitiesByRoutine({ id }) {
+  try {
+    const { rows: activities } = await client.query(
+      `
+      SELECT *
+      FROM routine_activities
+      WHERE "routineId"=$1;
+    `,
+      [id]
+    );
+
+    return activities;
+  } catch (err) {
+    throw err;
+  }
+}
+
 module.exports = {
   getRoutineActivityById,
   addActivityToRoutine,
